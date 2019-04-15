4c7ae6aa0f4b36ea1725c4f639bc3e6a72795556
root <root@ecsc00a03fc7.epam.com>
2019-04-15 12:05:57 +0000

    InfluxDB listener integration

--- scripts/classes/tiger_extensions.rb
+++ scripts/classes/tiger_extensions.rb
@@ -3,13 +3,13 @@ class TigerExtension
   require 'pathname'
   require 'fileutils'
 
-  def initialize (jmeter_jmx_path,tiger_extensions_path)
+  def initialize (jmeter_jmx_path,tiger_extension_path)
     @jmeter_jmx_path=jmeter_jmx_path
-    @tiger_extensions_path=tiger_extensions_path    
+    @tiger_extension_path=tiger_extension_path    
   end
 
   def extend_jmeter_jmx(data_folder_path)
-    $logger.info "Extending JMeter scenario #{@jmeter_jmx_path} with #{@tiger_extensions_path} content"
+    $logger.info "Extending JMeter scenario #{@jmeter_jmx_path} with #{@tiger_extension_path} content"
     tiger_extension=File.read(@tiger_extension_path)
     xml_cont=File.read(@jmeter_jmx_path).encode('UTF-8', :invalid => :replace)
     xml_ar=xml_cont.split(/<\/TestPlan>\W+<hashTree>/)
@@ -20,7 +20,7 @@ class TigerExtension
     File.write("#{jmx_path.dirname}/#{merged_file_name}",xml_modified)
     FileUtils.cp "#{jmx_path.dirname}/#{merged_file_name}",data_folder_path
     FileUtils.cp @jmeter_jmx_path,data_folder_path
-    FileUtils.cp "#{@tiger_extensions_path}/jmeter/tiger_extension.jmx",data_folder_path
+    FileUtils.cp @tiger_extension_path,data_folder_path
     return "#{jmx_path.dirname}/#{merged_file_name}"
   end
 
