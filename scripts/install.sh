82d55692d9b7138e568e89698da4306999ce2df1
ddn77 <42448807+ddn77@users.noreply.github.com>
2019-04-17 18:12:10 +0300

    Influx listener implementation (#5)
    * InfluxDB backend listener implementation

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,8 @@ set -e
 JM_VER="5.1.1"
 JM_NAME="apache-jmeter-${JM_VER}"
 JM_HOME_DIR=/opt/$JM_NAME
-JM_URL="https://archive.apache.org/dist/jmeter/binaries/${JM_NAME}.tgz" 
+JM_URL="https://archive.apache.org/dist/jmeter/binaries/${JM_NAME}.tgz"
+backend_listener_url="https://github.com/vvxxsz00/CompositeBackendListener.git" 
 
 export PATH=$PATH:${JM_HOME_DIR}/bin
 
@@ -18,7 +19,11 @@ rm -rf /var/cache/apk/*
 wget -O ${JM_NAME}.tgz ${JM_URL} 
 tar -xzf ${JM_NAME}.tgz -C /opt/ 
 rm -f ${JM_NAME}.tgz 
-mkdir -p /opt/tiger/scripts /opt/tiger/jmeter_test
+mkdir -p /opt/tiger/scripts /opt/tiger/jmeter_test /opt/tiger/temp
+git clone $backend_listener_url /opt/tiger/temp
 adduser -D -u 1001 tiger 
 chown -R tiger /opt/tiger
+mv /opt/tiger/temp/out/artifacts/InfluxBackendListener_jar/InfluxBackendListenerClient.jar /opt/apache-jmeter-5.1.1/lib/ext
+rm -rf /opt/tiger/temp
+
  
--- scripts/run_test.rb
+++ scripts/run_test.rb
@@ -1,39 +1,79 @@
 require 'yaml'
 require 'fileutils'
+require_relative 'classes/tiger_extensions.rb'
+require_relative 'classes/tiger_logger.rb'
 
-test_type='sample'
-puts "Clonning tests repository: git clone #{ENV['tests_repo']}"
-jmeter_test_path="/opt/tiger/jmeter_test"
-Dir.chdir jmeter_test_path
-raise "Tests were not downloaded successfully" unless system("git clone #{ENV['tests_repo']}")
+
+# defining variables
 tests_repo_name=ENV['tests_repo'].split('/').last.gsub('.git','')
+current_build_number=ENV['current_build_number'].to_i
+project_id=ENV['project_id']
+env_type=ENV['env_type']
+lg_id=ENV['lg_id']
 test_type=ENV['test_type']
-Dir.chdir("#{jmeter_test_path}/#{tests_repo_name}/#{test_type}")
-test_settings_hash=YAML.load(File.read("#{jmeter_test_path}/#{tests_repo_name}/#{test_type}/#{test_type}.yml"))
+jmeter_test_path="/opt/tiger/jmeter_test"
 test_results_folder="/opt/tiger/#{test_type}/results"
 data_folder=test_results_folder+"/data"
 logs_folder=test_results_folder+"/log"
 jmeter_cmd_options=''
-
 jmeter_bin_path='/opt/apache-jmeter-5.1.1/bin/jmeter'
+tiger_influxdb_extension_path='/opt/tiger/scripts/tiger_extensions/jmeter_tiger_extension.jmx'
 
+# creating folders
 [
   data_folder,
   logs_folder
 ].each {|folder_path| FileUtils.mkdir_p(folder_path) unless File.exists?(folder_path)}
 
+$logger=TigerLogger.new(logs_folder)
+
+$logger.info "Clonning tests repository: git clone #{ENV['tests_repo']}"
+Dir.chdir jmeter_test_path
+raise "Tests were not downloaded successfully" unless system("git clone #{ENV['tests_repo']}")
+Dir.chdir("#{jmeter_test_path}/#{tests_repo_name}/#{test_type}")
+
+
+
+test_settings_hash=YAML.load(File.read("#{jmeter_test_path}/#{tests_repo_name}/#{test_type}/#{test_type}.yml"))
+
+# reading tests settings from the YAML configuration file
+
+
+internal_jmeter_cmd_options_hash={
+      "build.id"=>"#{current_build_number}",
+      "report.csv"=>"#{data_folder}/#{test_type}_html_report.csv",
+      "errors.jtl"=>"#{data_folder}/#{test_type}_error.jtl",
+      "test.type"=>"#{test_type}",
+      "lg.id"=>"#{lg_id}",
+      "influx.protocol"=>"#{ENV['influx_protocol']}",
+      "influx.host"=>"#{ENV['influx_host']}",
+      "influx.port"=>"#{ENV['influx_port']}",
+      "influx.db"=>"#{ENV['influx_db']}",
+      "project.id"=>"#{project_id}",
+      "influx.username"=>"#{ENV['influx_username']}",
+      "influx.password"=>"#{ENV['influx_password']}",
+      "env.type"=>"#{env_type}"
+    }
+
+test_settings_hash['jmeter_args'].merge!(internal_jmeter_cmd_options_hash)
 test_settings_hash['jmeter_args'].each {|setting,value| jmeter_cmd_options+="-J#{setting}=#{value} "}
 
+tiger_extension_obj=TigerExtension.new(test_settings_hash['plan'],tiger_influxdb_extension_path)
+extended_jmeter_plan_path=tiger_extension_obj.extend_jmeter_jmx(data_folder)
+
+
+# compiling command line for the tests execution
 jmeter_cmd=[
   "#{jmeter_bin_path} -n",
-  "-t #{test_settings_hash['plan']}",
+  "-t #{extended_jmeter_plan_path}",
   "-p #{test_settings_hash['properties']}",
   jmeter_cmd_options.chomp,
   "-l #{data_folder}/#{test_type}.jtl",
   "-j #{logs_folder}/jmeter_#{test_type}.log"
 ].join(' ')
-puts "JMeter execution compiled command line: #{jmeter_cmd}"
+
+$logger.info "Launching JMeter using compiled command line: #{jmeter_cmd}"
 jmeter_cmd_res=system(jmeter_cmd)
-puts jmeter_cmd_res
-puts "Results folder: #{test_results_folder}"
+$logger.info jmeter_cmd_res
+$logger.info "Results folder: #{test_results_folder}"
 
