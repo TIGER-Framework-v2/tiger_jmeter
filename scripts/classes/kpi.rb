22aed56fc5821252b49834c6ebb05ae3f1600a8c
Anton Kaminski <anton_kaminski@epam.com>
2019-05-13 21:58:55 +0300

    Disabled ANY metric analyze

--- scripts/classes/kpi.rb
+++ scripts/classes/kpi.rb
@@ -25,10 +25,6 @@ class Kpi
       when /tps_rate/
         if (report_value < red_threshold)
           @red_threshold_violations_count += 1
-          $logger.info "Applying KPI settings for #{sampler_label_name}, #{scope_name}:"
-          $logger.info " |- Red threshold: #{red_threshold}"
-          $logger.info " |- Yellow threshold: #{yellow_threshold}"
-          $logger.info " |- Comparing with report data: #{report_value}"
         elsif (report_value < yellow_threshold)
           @yellow_threshold_violations_count += 1
         end
@@ -40,6 +36,10 @@ class Kpi
         end
       else
         if (report_value > red_threshold)
+        	$logger.info "Applying KPI settings for #{sampler_label_name}, #{scope_name}:"
+          $logger.info " |- Red threshold: #{red_threshold}"
+          $logger.info " |- Yellow threshold: #{yellow_threshold}"
+          $logger.info " |- Comparing with report data: #{report_value}"
           @red_threshold_violations_count += 1
         elsif (report_value > yellow_threshold)
           @yellow_threshold_violations_count += 1
@@ -62,17 +62,17 @@ class Kpi
         red_threshold      = @predefined_kpi[kpi_record_index][3]
         report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
         analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
-      elsif (@predefined_kpi['sampler_label'].include?('any'))
-        kpi_any_array = @predefined_kpi.select{|item| item[0]=='any'}
-        kpi_any_array.each do |item| # checking all the KPIs for all the 'any' statements in the KPI CSV file
-          $logger.info "Analyzing ANY statement of the KPI file: #{item}"
-          scope_name         = item[1]
-          report_scope_index = @aggregated_data_hash.headers.index(scope_name)
-          yellow_threshold   = item[2]
-          red_threshold      = item[3]
-          report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
-          analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
-        end
+      #elsif (@predefined_kpi['sampler_label'].include?('any'))
+      #  kpi_any_array = @predefined_kpi.select{|item| item[0]=='any'}
+      #  kpi_any_array.each do |item| # checking all the KPIs for all the 'any' statements in the KPI CSV file
+      #    $logger.info "Analyzing ANY statement of the KPI file: #{item}"
+      #    scope_name         = item[1]
+      #    report_scope_index = @aggregated_data_hash.headers.index(scope_name)
+      #    yellow_threshold   = item[2]
+      #    red_threshold      = item[3]
+      #    report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
+      #    analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
+      #  end
       end
     end
   end
