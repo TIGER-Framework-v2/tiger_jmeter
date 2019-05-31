45d17a787afe00e32af3ed8c17a672210c217f13
Anton Kaminski <anton_kaminski@epam.com>
2019-05-31 13:21:42 +0300

    KPI checking fixes. Added comparison of scope names. If regex matching KPI has the scope name as 'any' , then 'any' value will be excluded from analysing

--- scripts/classes/kpi.rb
+++ scripts/classes/kpi.rb
@@ -47,39 +47,91 @@ class Kpi
   def kpi_analyse
     @yellow_threshold_violations_count = 0
     @red_threshold_violations_count    = 0
+    
+    # Create array of all Any
+    if (@predefined_kpi['sampler_label'].include?('any'))
+      any_values_from_kpi = @predefined_kpi.select{|item| item[0] == 'any'}
+      any_values_from_kpi.delete_if {|row| row['scope_name'].nil? }                     # Delete empty elements from array
+    end
 
     @aggregated_data_hash['sampler_label'].each do |sampler_label_name|
-      report_sampler_index = @aggregated_data_hash['sampler_label'].index(sampler_label_name)
-
-      if (@predefined_kpi['sampler_label'].find { |e| /#{e}/ =~ sampler_label_name })
-        all_matches = @predefined_kpi.select{|item| /#{item[0]}/ =~ sampler_label_name }
-        all_matches.each do |item|
-          begin
-            scope_name         = item['scope_name']
-            report_scope_index = @aggregated_data_hash.headers.index(scope_name)
-            yellow_threshold   = item['yellow_threshold']
-            red_threshold      = item['red_threshold']
-            report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
-            analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
-          rescue 
-            p "Wrong 'sampler_label' defined in KPI's. Please check it for '#{sampler_label_name}'"
+    
+      p "Working with #{sampler_label_name}"
+   
+      selected_kpi  = Array.new
+      label_matches = @predefined_kpi.select{|item| /#{item[0]}/ =~ sampler_label_name } # Create array of all Matches
+      label_matches.delete_if {|row| row['scope_name'].nil? }                            # Delete empty elements from array
+      selected_kpi.push(*label_matches)
+    
+      if selected_kpi.length > 0 && !any_values_from_kpi.nil?                            # Delete Any value from analyse if it's scope name has already exist in label_matches array       
+        any_values_from_kpi.each do |item|
+          if label_matches.find { |el| el['scope_name'] == item['scope_name']}
+            $logger.warn "ANY '#{item['scope_name']}' has already set up for #{sampler_label_name}"
+          else
+            selected_kpi.push(item)
           end
         end
-      elsif (@predefined_kpi['sampler_label'].include?('any'))
-        kpi_any_array = @predefined_kpi.select{|item| item[0]=='any'}
-        kpi_any_array.each do |item| # checking all the KPIs for all the 'any' statements in the KPI CSV file
-          scope_name         = item[1]
-          report_scope_index = @aggregated_data_hash.headers.index(scope_name)
-          yellow_threshold   = item[2]
-          red_threshold      = item[3]
-          report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
-          analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
+      elsif !any_values_from_kpi.nil?                                                    # Add all 'any' values if no match was founded and 'any' KPI's were set up
+        selected_kpi.push(*any_values_from_kpi)    
+      end  
+   
+      report_sampler_index = @aggregated_data_hash['sampler_label'].index(sampler_label_name)
+
+      if selected_kpi.length > 0                                                         # Start analysing KPI's 
+        selected_kpi.each do |item|
+          begin
+            scope_name         = item['scope_name']
+            report_scope_index = aggregated_data_hash.headers.index(scope_name)
+            yellow_threshold   = item['yellow_threshold']
+            red_threshold      = item['red_threshold']
+            report_value       = aggregated_data_hash[report_sampler_index][report_scope_index]
+            analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
+          rescue 
+            $logger.error "Wrong 'scope_name' defined in KPI's. Please check it for '#{sampler_label_name}'"
+          end
         end
       end
     end
 
+
+
+
+
+
+    #@aggregated_data_hash['sampler_label'].each do |sampler_label_name|
+    #  report_sampler_index = @aggregated_data_hash['sampler_label'].index(sampler_label_name)
+#
+    #  if (@predefined_kpi['sampler_label'].find { |e| /#{e}/ =~ sampler_label_name })
+    #    all_matches = @predefined_kpi.select{|item| /#{item[0]}/ =~ sampler_label_name }
+    #    all_matches.each do |item|
+    #      begin
+    #        scope_name         = item['scope_name']
+    #        report_scope_index = @aggregated_data_hash.headers.index(scope_name)
+    #        yellow_threshold   = item['yellow_threshold']
+    #        red_threshold      = item['red_threshold']
+    #        report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
+    #        analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
+    #      rescue 
+    #        p "Wrong 'sampler_label' defined in KPI's. Please check it for '#{sampler_label_name}'"
+    #      end
+    #    end
+    #  elsif (@predefined_kpi['sampler_label'].include?('any'))
+    #    kpi_any_array = @predefined_kpi.select{|item| item[0]=='any'}
+    #    kpi_any_array.each do |item| # checking all the KPIs for all the 'any' statements in the KPI CSV file
+    #      scope_name         = item[1]
+    #      report_scope_index = @aggregated_data_hash.headers.index(scope_name)
+    #      yellow_threshold   = item[2]
+    #      red_threshold      = item[3]
+    #      report_value       = @aggregated_data_hash[report_sampler_index][report_scope_index]
+    #      analyze_metric(scope_name,sampler_label_name,red_threshold,yellow_threshold,report_value)
+    #    end
+    #  end
+    #end
+
     if @red_threshold_violations_count > 0
       p "Test has exceeded values"
+    else 
+      p "Test succeeded"
     end
   end
   
