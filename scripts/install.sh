c8b070921765f2972278ab3c0026ee51edaa3032
Dzmitry Dzerban <Dzmitry_Dzerban@epam.com>
2019-06-11 14:24:40 +0300

    Backend listener URL changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,7 @@ JM_VER="5.1.1"
 JM_NAME="apache-jmeter-${JM_VER}"
 JM_HOME_DIR=/opt/$JM_NAME
 JM_URL="https://archive.apache.org/dist/jmeter/binaries/${JM_NAME}.tgz"
-backend_listener_url="https://github.com/vvxxsz00/InfluxBackendListenerClient/blob/master/out/artifacts/InfluxBackendListener_jar/InfluxBackendListenerClient.jar"
-backend_listener_file_name="InfluxBackendListenerClient.jar"
+backend_listener_url="https://github.com/vvxxsz00/CompositeBackendListener.git" 
 
 export PATH=$PATH:${JM_HOME_DIR}/bin
 
@@ -22,10 +21,10 @@ wget -O ${JM_NAME}.tgz ${JM_URL}
 tar -xzf ${JM_NAME}.tgz -C /opt/ 
 rm -f ${JM_NAME}.tgz 
 mkdir -p /opt/tiger/scripts /opt/tiger/jmeter_test /opt/tiger/temp
-wget -O /opt/tiger/temp/$backend_listener_file_name $backend_listener_url
+git clone $backend_listener_url /opt/tiger/temp
 adduser -D -u 1001 tiger 
 chown -R tiger /opt/tiger
-mv /opt/tiger/temp/$backend_listener_file_name /opt/$JM_NAME/lib/ext
+mv /opt/tiger/temp/out/artifacts/InfluxBackendListener_jar/InfluxBackendListenerClient.jar /opt/apache-jmeter-5.1.1/lib/ext
 rm -rf /opt/tiger/temp
 
  
